a95866eb0fce36922c0d46814aea6c21e60b9ca2
Kauã da Silva Barbosa <kauadasilvabarbosa06@gmail.com>
2023-12-02 11:53:35 -0300

    Curso Finalizado
    Foram feitos a pagina com o album de fotos, ou para alguns LightBox.
    E o Slick para usar carousel

---
+++ js/17-plugin-slick.js
@@ -0,0 +1,11 @@
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    fade: true,
+    dots:true,
+    prevArrow: "<a class='voltar' href=''> < </a>",
+    nextArrow: "<a class='avancar' href=''> > </a>"
+  });
